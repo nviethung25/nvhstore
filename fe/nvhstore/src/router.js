2d0a7ce33ab42d7a51613c69c2d547e03de6ee95
NguyenVietHung25 <105406058+NguyenVietHung25@users.noreply.github.com>
2024-02-26 11:06:08 +0700

    Done b3

---
+++ fe/nvhstore/src/router.js
@@ -0,0 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/users/homePage";
+import { ROUTERS } from "./utils/router";
+import MasterLayout from "./pages/users/theme/masterLayout";
+import ProfilePage from "./pages/users/profilePage";
+
+const renderUserRouter = () => {
+  const userRouters = [
+    {
+      path: ROUTERS.USER.HOME,
+      component: <HomePage />,
+    },
+    {
+      path: ROUTERS.USER.PROFILE,
+      component: <ProfilePage />,
+    },
+  ];
+  return (
+    <MasterLayout>
+      <Routes>
+        {userRouters.map((item, key) => (
+          <Route key={key} path={item.path} element={item.component} />
+        ))}
+      </Routes>
+    </MasterLayout>
+  );
+};
+
+const RouterCustom = () => {
+  return renderUserRouter();
+};
+
+export default RouterCustom;
