34443a76ee0fcbf5d6e2ca0cd8c7906c365ec84c
NguyenVietHung25 <105406058+NguyenVietHung25@users.noreply.github.com>
2024-02-27 21:45:23 +0700

    Done b5

--- fe/nvhstore/src/pages/users/theme/header/index.js
+++ fe/nvhstore/src/pages/users/theme/header/index.js
@@ -1,10 +1,62 @@
 import { memo } from "react";
+import {
+  AiOutlineFacebook,
+  AiOutlineGooglePlus,
+  AiOutlineInstagram,
+  AiOutlineMail,
+  AiOutlineTwitter,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { Link } from "react-router-dom";
 import "./style.scss";
+import { fomatter } from "utils/fomater";
 
 const Header = () => {
   return (
     <div className="header__top">
-      <div className="container">Header</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 header__top_left">
+            <ul>
+              <li>
+                <AiOutlineMail />
+                hello@gmail.com
+              </li>
+              <li>Miễn phí ship đơn từ {fomatter(200000)}</li>
+            </ul>
+          </div>
+          <div className="col-6 header__top_right">
+            <ul>
+              <li>
+                <Link>
+                  <AiOutlineFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link>
+                  <AiOutlineInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link>
+                  <AiOutlineGooglePlus />
+                </Link>
+              </li>
+              <li>
+                <Link>
+                  <AiOutlineTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link>
+                  <AiOutlineUser />
+                  <span>Đăng nhập</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
